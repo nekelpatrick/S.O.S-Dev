4ebe655d90981c08e40f1ad90d555d0d3760ce24
Luan Lopes <luan@lopes-windows.local>
2021-01-11 14:33:21 -0300

    initial commit

---
+++ src/Pages/index.js
@@ -0,0 +1,17 @@
+import { Switch, Route } from 'react-router-dom'
+
+const Pages = () => {
+    //Importar header aqui;
+    return (
+        <Switch>
+            <Route exact path = '/'>
+                landing page
+            </Route>
+            <Route path = '/profile' >
+                profile
+            </Route>
+        </Switch>
+    )
+}
+
+export default Pages
