decd7eeec1b0ece3eb4980d7b7797f3a12b2c697
Gabriel Fabiano <gabrielfabianoo91@gmail.com>
2021-01-13 13:32:23 -0300

    fix: conflicts

--- src/Components/Atoms/Button/index.js
+++ src/Components/Atoms/Button/index.js
@@ -2,15 +2,16 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import useStyles from "./style.js";
 
-<<<<<<< HEAD
-export default function ContainedButtons({ text, classe, cor }) {
-=======
 export default function ContainedButtons({ text, classe, onClick }) {
->>>>>>> fee7ffda446f82ba3c4d7f7663c96da65f635ff2
   const classes = useStyles();
 
   return (
-    <Button className={classes[classe]} onClick = { onClick } variant="contained" color="secondary">
+    <Button
+      className={classes[classe]}
+      onClick={onClick}
+      variant="contained"
+      color="secondary"
+    >
       {text}
     </Button>
   );
