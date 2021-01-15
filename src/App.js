05b9d4da8022b05a133bb4476bbaa8a985f5b76b
Gabriel Fabiano <gabrielfabianoo91@gmail.com>
2021-01-15 16:32:52 -0300

    fix: conflicts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
-// import Pages from "./Pages/routes";
->>>>>>> bb719e9213606837a1a6f6b6490d572accaccc44
 import Header from "./Components/Organisms/Header/index";
 
 import { useStyles, theme } from "./Theme";
 
 import { ThemeProvider } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
-
 
 function App() {
   const classes = useStyles();
