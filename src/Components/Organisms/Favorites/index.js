13febb0024c0769ad6d98773280d9e097604dd82
Eduardo Vargas <jdrdcvrgs1@gmail.com>
2021-01-21 11:50:38 -0300

    fix: renderizando projetos filtrados se encontrar

--- src/Components/Organisms/Favorites/index.js
+++ src/Components/Organisms/Favorites/index.js
@@ -1,23 +1,9 @@
 import ProjectCard from "../../Molecules/Project-Card";
 
 import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
 
 const Favorites = () => {
-    const [favorites, setFavorites] = useState([]);
-    const [haveFavorites, setHaveFavorites] = useState(false);
     const { profile } = useSelector((state) => state)
-
-    // useEffect(() => {
-    //     if ( favorites.length < 1 ) {
-    //         setFavorites(profile.favorites)
-    //     }
-    //     if ( favorites.length > 0 ) {
-    //         setHaveFavorites(true)
-    //     };
-    // },[])
-
-    console.log(profile.favorites)
 
     return (
         <>
