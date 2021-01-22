2ba59539f215dfce551f81f0f88c2a793e148a88
Patrick <nekelpatrick@gmail.com>
2021-01-21 22:22:36 -0300

    fixing ui mobile on landing page

--- src/Pages/Landing-Page/index.js
+++ src/Pages/Landing-Page/index.js
@@ -33,10 +33,9 @@ const LandingPage = () => {
               <Comments name="" text="" src="" />
             </BoxComments>
           </Container>
+          <Slides />
         </>
       )}
-
-      <Slides />
     </>
   );
 };
